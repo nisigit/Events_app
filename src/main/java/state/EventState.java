77fa2553ee3a63a8e98f7742cc681884b68cfe25
Nisiman <s2100273@ed.ac.uk>
2022-03-26 17:33:06 +0000

    Removed EventState warnings

--- src/main/java/state/EventState.java
+++ src/main/java/state/EventState.java
@@ -11,24 +11,23 @@ public class EventState implements IEventState {
     private List<Event> events;
     private long uniqueEventNumber;
     private int performanceNumber;
-    private List<EventPerformance> eventPerformances;
 
 
     public EventState() {
         this.events = new ArrayList<>();
         this.performanceNumber = 1;
         this.uniqueEventNumber = 1;
-    };
+    }
 
     public EventState(IEventState other) {
         new EventState();
         this.events = other.getAllEvents();
-    };
+    }
 
     @Override
     public List<Event> getAllEvents() {
         return this.events;
-    };
+    }
 
     @Override
     public Event findEventByNumber(long eventNumber) {
@@ -38,27 +37,27 @@ public class EventState implements IEventState {
             }
         }
         return null;
-    };
+    }
 
     @Override
     public NonTicketedEvent createNonTicketedEvent(EntertainmentProvider organiser, String title, EventType type) {
         NonTicketedEvent nonTicketedEvent = new NonTicketedEvent(uniqueEventNumber++, organiser, title, type);
         events.add(nonTicketedEvent);
         return nonTicketedEvent;
-    };
+    }
 
     @Override
     public TicketedEvent createTicketedEvent(EntertainmentProvider organiser, String title, EventType type, double ticketPrice, int numTickets) {
         TicketedEvent ticketedEvent = new TicketedEvent(uniqueEventNumber++, organiser, title, type, ticketPrice, numTickets);
         events.add(ticketedEvent);
         return ticketedEvent;
-    };
+    }
 
     @Override
     public EventPerformance createEventPerformance(Event event, String venueAddress, LocalDateTime startDateTime, LocalDateTime endDateTime, List<String> performerNames, boolean hasSocialDistancing, boolean hasAirFiltration, boolean isOutdoors, int capacityLimit, int venueSize) {
         EventPerformance newPerformance =  new EventPerformance(performanceNumber++, event, venueAddress, startDateTime, endDateTime, performerNames, hasSocialDistancing, hasAirFiltration, isOutdoors, capacityLimit, venueSize);
         event.addPerformance(newPerformance);
         return newPerformance;
-    };
+    }
 
 }
