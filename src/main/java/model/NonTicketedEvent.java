7d1f43b92fb4bbd832a1b8c648a5c014c9b3c047
SamTCB <s2023979@ed.ac.uk>
2022-03-26 17:10:42 +0000

    Update NonTicketedEvent.java

--- src/main/java/model/NonTicketedEvent.java
+++ src/main/java/model/NonTicketedEvent.java
@@ -2,23 +2,13 @@ package model;
 
 public class NonTicketedEvent extends Event {
 
-    private long eventNumber;
-    private EntertainmentProvider organiser;
-    private String title;
-    private EventType type;
-
-
     public NonTicketedEvent(long eventNumber, EntertainmentProvider organiser, String title, EventType type) {
         super(eventNumber, organiser, title, type);
     };
 
     @Override
     public String toString() {
-        return "NonTicketedEvent{" +
-                "eventNumber=" + eventNumber +
-                ", organiser=" + organiser +
-                ", title='" + title + '\'' +
-                ", type=" + type +
-                '}';
+        return super.toString();
     }
 }
+
