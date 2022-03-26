c060744b212c1d9028e11f4dbfe5a87df6cb2513
Neel Amonkar <s2030247@ed.ac.uk>
2022-03-26 17:15:05 +0000

    worked on bookingState

--- src/main/java/state/BookingState.java
+++ src/main/java/state/BookingState.java
@@ -2,39 +2,50 @@ package state;
 
 import model.Booking;
 import model.EventPerformance;
+import model.Consumer;
 
-import java.util.function.Consumer;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 public class BookingState implements IBookingState {
 
-    public BookingState() {
+    private int nextBookingNumber = 0;
+    private ArrayList<Booking> bookings;
 
-    };
+    public BookingState() {
+        this.nextBookingNumber++;
+    }
 
     public BookingState(IBookingState other) {
-
-    };
+    }
 
     @Override
     public Booking findBookingByNumber(long bookingNumber) {
-
-    };
-
-    @Override
-    public Booking findBookingsByEventNumber(long eventNumber) {
-
-    }
-
-    @Override
-    public Booking createBooking(model.Consumer booker, EventPerformance performance, int numTickets, double amountPaid) {
+        for (Booking i: this.bookings) {
+            if (i.getBookingNumber() == bookingNumber) {
+                return i;
+            }
+        }
         return null;
     }
 
-    ;
-
     @Override
-    public Booking createBooking(Consumer booker, EventPerformance performance, int numTickets, double amountPaid) {
+    public List<Booking> findBookingsByEventNumber(long eventNumber) {
+        ArrayList<Booking> temp = new ArrayList<>();
+        for (Booking i : this.bookings) {
+            if (i.getEventPerformance().getEvent().getEventNumber() == eventNumber) {
+                temp.add(i);
+            }
+        }
+        return temp;
+    }
 
-    };
+    public Booking createBooking(Consumer booker, EventPerformance performance, int numTickets, double amountPaid) {
+        Booking newBooking = new Booking(this.nextBookingNumber, booker, performance,
+                                        numTickets, amountPaid, LocalDateTime.now());
+        this.nextBookingNumber++;
+        return newBooking;
+    }
 
 }
