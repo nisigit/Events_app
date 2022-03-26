d4d63612fc4b875b643467153afbbbb650da114f
Yifei-Wang28 <s2106632@ed.ac.uk>
2022-03-26 17:51:59 +0000

    GovernmentRepresentative correction

--- src/main/java/model/GovernmentRepresentative.java
+++ src/main/java/model/GovernmentRepresentative.java
@@ -1,9 +1,13 @@
-ackage model;
+package model;
 
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
 
 public class GovernmentRepresentative extends User {
+
+    private String email;
+    private String password;
+    private String paymentAccountEmail;
 
     public GovernmentRepresentative(String email, String password, String paymentAccountEmail) throws InvalidKeySpecException, NoSuchAlgorithmException {
         super(email, password, paymentAccountEmail);
