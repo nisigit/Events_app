c6cf7c569038798f1670f83546fb05b8869073cc
Yifei-Wang28 <s2106632@ed.ac.uk>
2022-03-26 17:13:01 +0000

    SponsorshipRequest updated

--- src/main/java/model/NonTicketedEvent.java
+++ src/main/java/model/NonTicketedEvent.java
@@ -2,13 +2,27 @@ package model;
 
 public class NonTicketedEvent extends Event {
 
+    private final long eventNumber;
+    private final EntertainmentProvider organiser;
+    private final String title;
+    private final EventType type;
+
+
     public NonTicketedEvent(long eventNumber, EntertainmentProvider organiser, String title, EventType type) {
-        super(eventNumber, organiser, title, type);
+        super();
+        this.eventNumber = eventNumber;
+        this.organiser = organiser;
+        this.title = title;
+        this.type = type;
     };
 
     @Override
     public String toString() {
-        return super.toString();
+        return "NonTicketedEvent{" +
+                "eventNumber=" + eventNumber +
+                ", organiser=" + organiser +
+                ", title='" + title + '\'' +
+                ", type=" + type +
+                '}';
     }
 }
-
