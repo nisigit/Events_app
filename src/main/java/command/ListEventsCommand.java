5dc5f1fbce5b0496167b899687c9652747f1b4fa
Neel Amonkar <s2030247@ed.ac.uk>
2022-03-30 20:53:33 +0100

    changes to ListEventsCommand

--- src/main/java/command/ListEventsCommand.java
+++ src/main/java/command/ListEventsCommand.java
@@ -11,52 +11,75 @@ public class ListEventsCommand implements ICommand {
     private boolean activeEventsOnly;
     private List<Event> result;
 
-
     public ListEventsCommand(boolean userEventsOnly, boolean activeEventsOnly) {
         this.userEventsOnly = userEventsOnly;
         this.activeEventsOnly = activeEventsOnly;
     }
 
-
-
     @Override
     public void execute(Context context) {
-        result = null;
+        // Sam: changed implementation a bit - tidied up some indentation and also amended activeEventsOnly case
+        // seemed to me as though with the way it was originally implemented, result would have non-user events
+        // even if userEventsOnly was true
+
         User user = context.getUserState().getCurrentUser();
         List<Event> allEvents = context.getEventState().getAllEvents();
 
-        if (this.userEventsOnly && (user == null)) return;
-        else if (this.userEventsOnly && (user instanceof EntertainmentProvider)) {
-            result = ((EntertainmentProvider) user).getEvents();
-        } else if (this.userEventsOnly && (user instanceof Consumer)) {
-            ConsumerPreferences cp = ((Consumer) user).getPreferences();
-            for (Event event : allEvents) {
-                EventPerformance ep = event.getPerformances().iterator().next();
-                if ((ep.hasAirFiltration() == cp.preferAirFiltration()) &&
-                        (ep.isOutdoors() == cp.preferAirFiltration()) &&
-                        (ep.hasSocialDistancing() == cp.preferSocialDistancing()) &&
-                        (ep.getCapacityLimit() <= cp.preferredMaxCapacity()) &&
-                        (ep.getVenueSize() <= cp.preferredMaxVenueSize())) {
-                    result.add(event);
-                } else {
-                    return;
-                }
+        if (user == null) return;
+        if (this.userEventsOnly) {
+            if (user instanceof EntertainmentProvider) {
+                result = ((EntertainmentProvider) user).getEvents();
             }
-            if (this.activeEventsOnly) {
+            else if (user instanceof Consumer) {
+                ConsumerPreferences cp = ((Consumer) user).getPreferences();
                 for (Event event : allEvents) {
-                    if (event.getStatus() == EventStatus.ACTIVE) {
+                    EventPerformance ep = event.getPerformances().iterator().next();
+                    if ((ep.hasAirFiltration() == cp.preferAirFiltration()) &&
+                            (ep.isOutdoors() == cp.preferAirFiltration()) &&
+                            (ep.hasSocialDistancing() == cp.preferSocialDistancing()) &&
+                            (ep.getCapacityLimit() <= cp.preferredMaxCapacity()) &&
+                            (ep.getVenueSize() <= cp.preferredMaxVenueSize())) {
                         result.add(event);
-
                     }
                 }
             }
         }
-    }
+        else result = allEvents;
 
-        @Override
-        public List<Event> getResult() {
-            return result;
+        if (this.activeEventsOnly) {
+            result.removeIf(event -> event.getStatus() != EventStatus.ACTIVE);
         }
 
+//            if (this.userEventsOnly && (user == null)) return;
+//            else if (this.userEventsOnly && (user instanceof EntertainmentProvider)) {
+//                result = ((EntertainmentProvider) user).getEvents();
+//            } else if (this.userEventsOnly && (user instanceof Consumer)) {
+//                ConsumerPreferences cp = ((Consumer) user).getPreferences();
+//                for (Event event : allEvents) {
+//                    EventPerformance ep = event.getPerformances().iterator().next();
+//                    if ((ep.hasAirFiltration() == cp.preferAirFiltration()) &&
+//                            (ep.isOutdoors() == cp.preferAirFiltration()) &&
+//                            (ep.hasSocialDistancing() == cp.preferSocialDistancing()) &&
+//                            (ep.getCapacityLimit() <= cp.preferredMaxCapacity()) &&
+//                            (ep.getVenueSize() <= cp.preferredMaxVenueSize())) {
+//                        result.add(event);
+//                    } else {
+//                        return;
+//                    }
+//                }
+//                if (this.activeEventsOnly) {
+//                    for (Event event : allEvents) {
+//                        if (event.getStatus() == EventStatus.ACTIVE) {
+//                            result.add(event);
+//                        }
+//                    }
+//                }
+//            }
+//        }
+    }
 
+    @Override
+    public List<Event> getResult() {
+        return result;
+    }
 }
