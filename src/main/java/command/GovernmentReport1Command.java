9049ae1fc24f9b75ea92e68c426aeace8c7c7e76
Yifei-Wang28 <s2106632@ed.ac.uk>
2022-03-30 17:06:39 +0100

    GovernmentReport1Command constructor added

--- src/main/java/command/GovernmentReport1Command.java
+++ src/main/java/command/GovernmentReport1Command.java
@@ -8,9 +8,12 @@ import java.time.LocalDateTime;
 public class GovernmentReport1Command implements ICommand {
 
     private Booking result;
+    private LocalDateTime intervalStartInclusive;
+    private LocalDateTime intervalEndInclusive;
 
     public GovernmentReport1Command(LocalDateTime intervalStartInclusive, LocalDateTime intervalEndInclusive) {
-
+        this.intervalEndInclusive = intervalEndInclusive;
+        this.intervalEndInclusive = intervalEndInclusive;
     };
 
     @Override
