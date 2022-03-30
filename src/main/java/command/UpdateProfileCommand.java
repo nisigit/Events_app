a72ea64f64e9e82370f7a64d29526b9e919fdf75
Neel Amonkar <s2030247@ed.ac.uk>
2022-03-30 17:34:23 +0100

    first version implementation of UpdateProfileCommand

--- src/main/java/command/UpdateProfileCommand.java
+++ src/main/java/command/UpdateProfileCommand.java
@@ -1,24 +1,27 @@
 package command;
 
 import controller.Context;
+import model.User;
 
 public abstract class UpdateProfileCommand implements ICommand {
 
-    protected Boolean successResult() {
-
-    }
+    protected Boolean successResult;
 
     public UpdateProfileCommand() {
-
     }
 
     protected boolean isProfileUpdateInvalid(Context context, String oldPassword, String newEmail) {
-
+        User user = context.getUserState().getCurrentUser();
+        if (user.checkPasswordMatch(oldPassword)) {
+            user.setEmail(newEmail);
+            return true;
+        }
+        else return false;
     }
 
     @Override
     public Boolean getResult() {
-
+        return successResult;
     }
 
 }
