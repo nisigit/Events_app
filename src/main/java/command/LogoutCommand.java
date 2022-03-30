c28806536cab0a0bd563319413b9ce495d4f8c00
Nisiman <s2100273@ed.ac.uk>
2022-03-30 17:42:43 +0100

    LogoutCommand done

--- src/main/java/command/LogoutCommand.java
+++ src/main/java/command/LogoutCommand.java
@@ -1,6 +1,8 @@
 package command;
 
 import controller.Context;
+import model.User;
+import state.IUserState;
 
 public class LogoutCommand implements ICommand {
 
@@ -10,12 +12,17 @@ public class LogoutCommand implements ICommand {
 
     @Override
     public void execute(Context context) {
-
+        IUserState userState = context.getUserState();
+        User user = userState.getCurrentUser();
+        if (user == null) {
+            return;
+        }
+        userState.setCurrentUser(null);
     }
 
     @Override
     public Void getResult() {
-
+        return null;
     }
 
 }
