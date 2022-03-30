7fdeea6306f874b266e6820fe0a5702690f794f3
Neel Amonkar <s2030247@ed.ac.uk>
2022-03-30 16:56:36 +0100

    implemented GetAvailablePerformanceTicketsCommand

--- src/main/java/command/GetAvailablePerformanceTicketsCommand.java
+++ src/main/java/command/GetAvailablePerformanceTicketsCommand.java
@@ -1,19 +1,39 @@
 package command;
 
+import controller.Context;
+import model.Event;
+import model.EventPerformance;
+import model.TicketedEvent;
+
 public class GetAvailablePerformanceTicketsCommand extends Object implements ICommand {
 
+    private long eventNumber, performanceNumber;
+    private int result;
     public GetAvailablePerformanceTicketsCommand(long eventNumber, long performanceNumber) {
-
-    };
+        this.eventNumber = eventNumber;
+        this.performanceNumber = performanceNumber;
+    }
 
     @Override
     public void execute(Context context) {
+        this.result = -1;
+        Event event = context.getEventState().findEventByNumber(this.eventNumber);
 
-    };
+        if (event == null) return;
+
+        if (event instanceof TicketedEvent) {
+            EventPerformance performance = event.getPerformanceByNumber(this.performanceNumber);
+
+            if (performance == null) return;
+
+            result = event.getOrganiser().getProviderSystem().getNumTicketsLeft(this.eventNumber, this.performanceNumber);
+        }
+        else return;
+    }
 
     @Override
     public Integer getResult() {
-
-    };
+        return result;
+    }
 
 }
