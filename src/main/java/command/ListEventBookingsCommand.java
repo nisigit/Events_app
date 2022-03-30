c1a034687cc8886886800677b4c715b319f7476b
Neel Amonkar <s2030247@ed.ac.uk>
2022-03-30 17:13:42 +0100

    implemented ListEventsBookingCommand

--- src/main/java/command/ListEventBookingsCommand.java
+++ src/main/java/command/ListEventBookingsCommand.java
@@ -1,22 +1,35 @@
 package command;
 
 import controller.Context;
-import model.Booking;
+import model.*;
+
+import java.util.List;
 
 public class ListEventBookingsCommand implements ICommand {
 
-    public ListEventBookingsCommand(long eventNumber) {
+    private long eventNumber;
+    private List<Booking> result;
 
+    public ListEventBookingsCommand(long eventNumber) {
+        this.eventNumber = eventNumber;
     }
 
     @Override
     public void execute(Context context) {
-
+        result = null;
+        User user = context.getUserState().getCurrentUser();
+        Event event = context.getEventState().findEventByNumber(this.eventNumber);
+        if (event == null) return;
+        if (!(event instanceof TicketedEvent)) return;
+        if (user instanceof GovernmentRepresentative || user.equals(event.getOrganiser())) {
+            result = context.getBookingState().findBookingsByEventNumber(this.eventNumber);
+        }
+        else return;
     }
 
     @Override
-    public Booking getResult() {
-
+    public List<Booking> getResult() {
+        return result;
     }
 
 }
