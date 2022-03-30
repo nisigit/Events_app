46ad511413d0e839746f66b6055bd839d64a2eb0
Nisiman <s2100273@ed.ac.uk>
2022-03-30 16:27:04 +0100

    CreateNonTicketedEventCommand done

--- src/main/java/command/CreateNonTicketedEventCommand.java
+++ src/main/java/command/CreateNonTicketedEventCommand.java
@@ -1,16 +1,28 @@
 package command;
 
 import controller.Context;
+import model.EntertainmentProvider;
 import model.EventType;
+import model.NonTicketedEvent;
+import model.User;
+import state.IEventState;
 
 public class CreateNonTicketedEventCommand extends CreateEventCommand {
 
     public CreateNonTicketedEventCommand(String title, EventType type) {
         super(title, type);
-    };
+    }
 
     public void execute(Context context) {
+        User user = context.getUserState().getCurrentUser();
+        if (!(user instanceof EntertainmentProvider)) {
+            return;
+        }
 
-    };
+        IEventState eventState = context.getEventState();
+        NonTicketedEvent nonTicketedEvent = eventState.createNonTicketedEvent((EntertainmentProvider) user, this.title,
+                this.type);
 
+        this.eventNumberResult = nonTicketedEvent.getEventNumber();
+    }
 }
