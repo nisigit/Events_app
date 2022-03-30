76177686a49257b7620f7994ffbb1343afac680a
Yifei-Wang28 <s2106632@ed.ac.uk>
2022-03-30 17:31:49 +0100

    Complete ListSponsorshipRequestsCommand

--- src/main/java/command/ListSponsorshipRequestsCommand.java
+++ src/main/java/command/ListSponsorshipRequestsCommand.java
@@ -2,21 +2,32 @@ package command;
 
 import controller.Context;
 import model.SponsorshipRequest;
+import model.*;
+
+import java.util.*;
 
 public class ListSponsorshipRequestsCommand implements ICommand {
 
-    public ListSponsorshipRequestsCommand(boolean pendingRequestsOnly) {
+    private boolean pendingRequestsOnly;
+    private List<SponsorshipRequest> result;
 
+    public ListSponsorshipRequestsCommand(boolean pendingRequestsOnly) {
+        this.pendingRequestsOnly = pendingRequestsOnly;
     }
 
     @Override
     public void execute(Context context) {
-
+        User currentUser = context.getUserState().getCurrentUser();
+        if (currentUser == null) return;
+        if (!(currentUser instanceof GovernmentRepresentative)) return;
+        else {
+            result = context.getSponsorshipState().getAllSponsorshipRequests();
+        };
     }
 
     @Override
-    public SponsorshipRequest getResult() {
-
+    public List<SponsorshipRequest> getResult() {
+        return result;
     }
 
 }
