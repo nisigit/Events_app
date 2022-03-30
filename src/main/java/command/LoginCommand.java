d3854730f3d90effc507ba390caac04ba0129c77
Nisiman <s2100273@ed.ac.uk>
2022-03-30 17:31:41 +0100

    LoginCommand done

--- src/main/java/command/LoginCommand.java
+++ src/main/java/command/LoginCommand.java
@@ -3,20 +3,34 @@ package command;
 import controller.Context;
 import model.User;
 
-public class LoginCommand extends Object implements ICommand {
+public class LoginCommand implements ICommand {
+
+    private String email;
+    private String password;
+    private User user;
 
     public LoginCommand(String email, String password) {
-
-    };
+        this.email = email;
+        this.password = password;
+        user = null;
+    }
 
     @Override
     public void execute(Context context) {
+        user = context.getUserState().getAllUsers().get(email);
 
-    };
+        if (user == null) {
+            return;
+        }
+
+        if (!user.checkPasswordMatch(password)) {
+            user = null;
+        }
+    }
 
     @Override
     public User getResult() {
-
-    };
+        return user;
+    }
 
 }
