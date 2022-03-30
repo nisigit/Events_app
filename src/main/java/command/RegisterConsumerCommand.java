3816da16eb7ba08513592442bd7491d8a4e20943
Nisiman <s2100273@ed.ac.uk>
2022-03-30 17:51:47 +0100

    Removed unnecessary exceptions

--- src/main/java/command/RegisterConsumerCommand.java
+++ src/main/java/command/RegisterConsumerCommand.java
@@ -3,27 +3,22 @@ package command;
 import controller.Context;
 import model.Consumer;
 
-import java.security.NoSuchAlgorithmException;
-import java.security.spec.InvalidKeySpecException;
-
 public class RegisterConsumerCommand implements ICommand {
 
     // TODO: What are we supposed to do with this consumer instance? How does it "login".
-    public RegisterConsumerCommand(String name, String email, String phoneNumber, String password, String paymentAccountEmail)
-            throws InvalidKeySpecException, NoSuchAlgorithmException {
+    public RegisterConsumerCommand(String name, String email, String phoneNumber, String password, String paymentAccountEmail) {
         super();
         Consumer consumer = new Consumer(name, email, phoneNumber, password, paymentAccountEmail);
-    };
+    }
 
-    // TODO: No idea what this is.
     @Override
     public void execute(Context context) {
 
-    };
+    }
 
     @Override
     public Consumer getResult() {
 
-    };
+    }
 
 }
--- src/main/java/model/Consumer.java
+++ src/main/java/model/Consumer.java
@@ -1,7 +1,5 @@
 package model;
 
-import java.security.NoSuchAlgorithmException;
-import java.security.spec.InvalidKeySpecException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,46 +10,45 @@ public class Consumer extends User {
     private ArrayList<Booking> bookings;
     private ConsumerPreferences prefs;
 
-    public Consumer(String name, String email, String phoneNumber, String password, String paymentAccountEmail)
-            throws InvalidKeySpecException, NoSuchAlgorithmException {
+    public Consumer(String name, String email, String phoneNumber, String password, String paymentAccountEmail) {
         super(email, password, paymentAccountEmail);
         this.name = name;
         this.phoneNumber = phoneNumber;
         this.prefs = new ConsumerPreferences();
         this.bookings = new ArrayList<>();
-    };
+    }
 
     public void addBooking(Booking booking) {
         this.bookings.add(booking);
-    };
+    }
 
     public String getName() {
         return this.name;
-    };
+    }
 
     public ConsumerPreferences getPreferences() {
         return this.prefs;
-    };
+    }
 
     public void setPreferences(ConsumerPreferences preferences) {
         this.prefs = preferences;
-    };
+    }
 
     public List<Booking> getBookings() {
         return bookings;
-    };
+    }
 
     public void notify(String message) {
 
-    };
+    }
 
     public void setName(String newName) {
 
-    };
+    }
 
     public void setPhoneNumber(String newPhoneNumber) {
 
-    };
+    }
 
     @Override
     public String toString() {
