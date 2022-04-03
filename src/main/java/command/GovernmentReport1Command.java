cf0f6b71c5227ed105b6b635a698e7f73080082d
Yifei-Wang28 <s2106632@ed.ac.uk>
2022-04-03 16:36:22 +0100

    Javadoc for GovernmentReport1Command

--- src/main/java/command/GovernmentReport1Command.java
+++ src/main/java/command/GovernmentReport1Command.java
@@ -13,11 +13,24 @@ public class GovernmentReport1Command implements ICommand {
     private List<Booking> result;
     private LocalDateTime intervalStartInclusive, intervalEndInclusive;
 
+    /**
+     *
+     * @param intervalStartInclusive indicates the start time of the interval provided
+     * @param intervalEndInclusive indicates the end time of the interval provided
+     */
+
     public GovernmentReport1Command(LocalDateTime intervalStartInclusive, LocalDateTime intervalEndInclusive) {
         this.intervalStartInclusive = intervalStartInclusive;
         this.intervalEndInclusive = intervalEndInclusive;
         this.result = new ArrayList<>();
     }
+
+    /**
+     * This method should not be called directly outside of testing.
+     * @param context object that provides access to global application state
+     * Verifies that:
+     *
+     */
 
     @Override
     public void execute(Context context) {
@@ -51,6 +64,11 @@ public class GovernmentReport1Command implements ICommand {
         }
 
     }
+
+    /**
+     * Get the result from the latest run of the command.
+     * @return
+     */
 
     @Override
     public List<Booking> getResult() {
