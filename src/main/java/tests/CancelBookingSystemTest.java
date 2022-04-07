8a83d70b232d1551268299714cd7fcaa174933da
Yifei-Wang28 <s2106632@ed.ac.uk>
2022-04-07 22:32:30 +0100

    Add CancelBookingSystemTest

---
+++ src/main/java/tests/CancelBookingSystemTest.java
@@ -0,0 +1,33 @@
+package tests;
+
+import command.*;
+import controller.Controller;
+import logging.Logger;
+import model.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class CancelBookingSystemTest {
+    @BeforeEach
+    void printTestName(TestInfo testInfo) {
+        System.out.println(testInfo.getDisplayName());
+    }
+
+    @AfterEach
+    void clearLogs() {
+        Logger.getInstance().clearLog();
+        System.out.println("---");
+    }
+
+    @Test
+    void cancelEventTest() {
+
+    }
+}
