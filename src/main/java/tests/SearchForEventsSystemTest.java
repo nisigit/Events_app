458b7bcfd6fa00c34a9e5624cb25cfa869a72b42
Yifei-Wang28 <s2106632@ed.ac.uk>
2022-04-14 19:28:57 +0100

    Change system tests' names

--- src/main/java/tests/SearchForEventsSystemTest.java
+++ src/main/java/tests/SearchForEventsSystemTest.java
@@ -14,7 +14,7 @@ import java.time.LocalDateTime;
 import java.util.*;
 import static org.testng.Assert.assertEquals;
 
-public class SearchForEventsTest {
+public class SearchForEventsSystemTest {
     @BeforeEach
     void printTestName(TestInfo testInfo) {
         System.out.println(testInfo.getDisplayName());
