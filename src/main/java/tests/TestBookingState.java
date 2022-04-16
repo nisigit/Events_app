54f66cfed824e3de096f54dc5db31e718698288a
Yifei-Wang28 <s2106632@ed.ac.uk>
2022-04-16 17:14:46 +0100

    Adding assertion messages to TestBookingState

--- src/main/java/tests/TestBookingState.java
+++ src/main/java/tests/TestBookingState.java
@@ -43,12 +43,12 @@ public class TestBookingState {
 
     @Test
     void TestConstructors() {
-        assertEquals(bookingState0.getBookings(), new HashMap<>());
-        assertEquals(bookingState0.getNextBookingNumber(), 1);
+        assertEquals(bookingState0.getBookings(), new HashMap<>(), "Should initialized as empty hash map");
+        assertEquals(bookingState0.getNextBookingNumber(), 1, "Should be initialized as 1");
 
         BookingState bookingState1 = new BookingState(bookingState0);
-        assertEquals(bookingState1, bookingState0);
-        assertNotSame(bookingState1, bookingState0);
+        assertEquals(bookingState1, bookingState0, "Should have the same values");
+        assertNotSame(bookingState1, bookingState0, "Should be a deep copy");
     }
 
     @Test
@@ -62,9 +62,9 @@ public class TestBookingState {
         Booking booking12 = new Booking(1, consumer1, eventPerformance1, 2, 17.76, time1);
         Booking booking22 = new Booking(2, consumer1, eventPerformance1, 4, 35.52, time2);
         Booking booking32 = new Booking(3, consumer1, eventPerformance2, 1, 20, time3);
-        assertEquals(booking1, booking12);
-        assertEquals(booking2, booking22);
-        assertEquals(booking3, booking32);
+        assertEquals(booking1, booking12, "Should be equal with same fields");
+        assertEquals(booking2, booking22, "Should be equal with same fields");
+        assertEquals(booking3, booking32, "Should be equal with same fields");
     }
 
     @Test
@@ -72,10 +72,10 @@ public class TestBookingState {
         Booking booking1 = bookingState0.createBooking(consumer1, eventPerformance1, 2, 17.76);
         Booking booking2 = bookingState0.createBooking(consumer1, eventPerformance1, 4, 35.52);
         Booking booking3 = bookingState0.createBooking(consumer1, eventPerformance2, 1, 20);
-        assertEquals(bookingState0.findBookingByNumber(1), booking1);
-        assertEquals(bookingState0.findBookingByNumber(2), booking2);
-        assertEquals(bookingState0.findBookingByNumber(3), booking3);
-        assertNull(bookingState0.findBookingByNumber(4));
+        assertSame(bookingState0.findBookingByNumber(1), booking1, "Should be the same object");
+        assertSame(bookingState0.findBookingByNumber(2), booking2, "Should be the same object");
+        assertSame(bookingState0.findBookingByNumber(3), booking3, "Should be the same object");
+        assertNull(bookingState0.findBookingByNumber(4), "Should be null with non-existed booking number");
     }
 
     @Test
@@ -83,20 +83,20 @@ public class TestBookingState {
         Booking booking1 = bookingState0.createBooking(consumer1, eventPerformance1, 2, 17.76);
         Booking booking2 = bookingState0.createBooking(consumer1, eventPerformance1, 4, 35.52);
         Booking booking3 = bookingState0.createBooking(consumer1, eventPerformance2, 1, 20);
-        assertEquals(bookingState0.findBookingsByEventNumber(1), List.of(booking1, booking2));
-        assertEquals(bookingState0.findBookingsByEventNumber(2), List.of(booking3));
-        assertNull(bookingState0.findBookingByNumber(4));
+        assertEquals(bookingState0.findBookingsByEventNumber(1), List.of(booking1, booking2), "Should give a list of first 2 bookings");
+        assertEquals(bookingState0.findBookingsByEventNumber(2), List.of(booking3), "Should give a list of the last booking");
+        assertNull(bookingState0.findBookingByNumber(4), "Should be null with non-existed booking number");
     }
 
     @Test
     void TestGetNextBookingNumber() {
-        assertEquals(bookingState0.getNextBookingNumber(), 1);
+        assertEquals(bookingState0.getNextBookingNumber(), 1, "Initialized as 1");
         Booking booking1 = bookingState0.createBooking(consumer1, eventPerformance1, 2, 17.76);
-        assertEquals(bookingState0.getNextBookingNumber(), 2);
+        assertEquals(bookingState0.getNextBookingNumber(), 2, "Should be 2 after 1 booking is created");
         Booking booking2 = bookingState0.createBooking(consumer1, eventPerformance1, 4, 35.52);
-        assertEquals(bookingState0.getNextBookingNumber(), 3);
+        assertEquals(bookingState0.getNextBookingNumber(), 3, "Should be 3 after 2 bookings are created");
         Booking booking3 = bookingState0.createBooking(consumer1, eventPerformance2, 1, 20);
-        assertEquals(bookingState0.getNextBookingNumber(), 4);
+        assertEquals(bookingState0.getNextBookingNumber(), 4, "Should be 4 after 3 bookings are created");
     }
 
     @Test
@@ -107,7 +107,7 @@ public class TestBookingState {
         HashMap<Long, List<Booking>> expectedBookings = new HashMap<>();
         expectedBookings.put(Integer.toUnsignedLong(1), List.of(booking1, booking2));
         expectedBookings.put(Integer.toUnsignedLong(2), List.of(booking3));
-        assertEquals(bookingState0.getBookings(), expectedBookings);
+        assertEquals(bookingState0.getBookings(), expectedBookings, "Should give the same result as the expected Hash map");
     }
 
 }
