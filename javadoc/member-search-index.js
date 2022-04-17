eec12d5c95090917bc46a525faafc2cc738742e5
Neel Amonkar <s2030247@ed.ac.uk>
2022-04-17 17:23:55 +0100

    ACTUALLY added javadoc for that command

---
+++ javadoc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"command","c":"GovernmentReport1Command","l":"execute(Context)","url":"execute(controller.Context)"},{"p":"command","c":"GovernmentReport1Command","l":"getResult()"},{"p":"command","c":"GovernmentReport1Command","l":"GovernmentReport1Command(LocalDateTime, LocalDateTime)","url":"%3Cinit%3E(java.time.LocalDateTime,java.time.LocalDateTime)"}]
